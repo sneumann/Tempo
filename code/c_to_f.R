3737bd4e705bd3045259fbb5a85a0872cd188ff3
georgejr <methungeorge333@gmail.com>
2024-06-13 17:09:21 +0200

    todo : change on code

--- code/c_to_f.R
+++ code/c_to_f.R
@@ -1,3 +1,11 @@
+
+#' Convert Celsius to Fahrenheit
+#'
+#' @param c Temperature in Celsius
+#' @return Equivalent temperature in Fahrenheit
+#' @export
+
+
 C_to_F <- function(C_temp){
   F_temp <- (C_temp * 9/5 ) + 32:
     return (F_temp)
--- code/f_to_c.R
+++ code/f_to_c.R
@@ -1,3 +1,10 @@
+#' Convert Fahrenheit to Celsius
+#'
+#' @param f Temperature in Fahrenheit
+#' @return Equivalent temperature in Celsius
+#' @export
+
+
 F_to_C <- function(F_temp){
   C_temp <- (F_temp - 32) * 5/9:
     return(C_temp)
